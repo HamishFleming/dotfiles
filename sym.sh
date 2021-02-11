4205295fc971223b02adab01bf0a4de2e25b8d20
Hamish Fleming <fleming_hamish@yahoo.com>
2021-02-11 17:21:42 +1100

    vimrc symlink

--- sym.sh
+++ sym.sh
@@ -10,5 +10,7 @@ ln -sf ~/.dotfiles/my-theme.zsh-theme ~/.oh-my-zsh/themes/my-theme.zsh-theme
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 #awesomerc
 #ln -sf ~/.dotfiles/rc.lua ~/.config/awesome/rc.lua
+#Vimrc
+ln -sf ~/.dotfiles/.vimrc  ~/.vimrc  
 
 
